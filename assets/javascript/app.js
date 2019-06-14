400387e7408f631d7ec20d8871f5124c9b24f8c0
Bri <breezy.aguilar@gmail.com>
2019-06-14 14:54:52 -0500

    Question now gets pushed to 'question div'

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,60 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-// Setting variables
-var trivia = $("#gamePage")
+    // Setting global variables
+    var trivia = $("#gamePage")
+
+    // Setting choice variables
+    var choiceA = $("#A")
+    var choiceB = $("#B")
+    var choiceC = $("#C")
+
+
+
+
+    // Setting questions variable
+
+
+    var questions = [
+        {   quest: "Who is the lead singer of Third Eye Blind?",
+            choiceA: "Stephen Jenkins",
+            choiceB: "Dave Grohl",
+            choiceC: "Kurt Cobain",
+            correct: "A"
+        },
+        { quest: "Who is the lead singer of the Foo Fighters?",
+          choiceA: "Stephen Jenkins",
+          choiceB: "Dave Grohl",
+          choiceC: "Kurt Cobain",
+          correct: "B"
+        }
+    ]
+
+    var lastQuestionIndex = questions.length - 1;
+    var runningQuestionIndex = 0;
+
+    // Creating a renderQuestion function
+    function renderQuestion() {
+        var q = questions[runningQuestionIndex]
+
+        $("#question").append("<p>" + q.quest + "</p>");
+    }
+
+
+
+
 
     // "onclick" of the start button
-$("#starter").on("click", function() {
-    // Display only "trivia page" on click
-    $("#gamePage").show();
-    $("#start").hide();
-    $("#score").hide();
+    $("#starter").on("click", function startQuiz() {
+        // Display only "trivia page" on click
+        $("#start").hide();
+        $("#gamePage").show();
+        $("#score").hide();
 
-    // Display question
-    $("#question")
+        renderQuestion();
+    });
+
+
+        
+        
+
 })
-});
